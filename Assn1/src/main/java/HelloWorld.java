c3559569cc400ef2adafef922a7094a65187e955
Joshua Hawks <ja.ne.hw.1986@gmail.com>
2017-09-29 09:49:51 -0700

    Updated files with user input to get the name. The other purpose for this was to test changes in Intellij to see if it would properly change the contributor.

--- Assn1/src/main/java/HelloWorld.java
+++ Assn1/src/main/java/HelloWorld.java
@@ -5,10 +5,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
+        //Intro text to user writing their name.
         System.out.println("Hello World");
         System.out.println("Please enter your name: ");
+
+        //User input for their name.
         String name = userInput.nextLine();
 
+        //Displays user name input.
         System.out.println("Your name is: " + name);
 
         userInput.close();
