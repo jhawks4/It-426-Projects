52401c9e1babbb6ed1ac412cd7994ca997a3ecfc
Joshua Hawks <ja.ne.hw.1986@gmail.com>
2017-09-29 09:40:42 -0700

    The files from the project including a single HelloWorld java file in main.

--- Assn1/src/main/java/HelloWorld.java
+++ Assn1/src/main/java/HelloWorld.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
         System.out.println("Hello World");
+        System.out.println("Please enter your name: ");
+        String name = userInput.nextLine();
+
+        System.out.println("Your name is: " + name);
+
+        userInput.close();
+
+
     }
 }
