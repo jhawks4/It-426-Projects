a7e7f67a8321fd6ddc565fe425e4ff6c51245cfe
Joshua Hawks <jhawks4@mail.greenriver.edu>
2017-12-01 05:36:13 -0800

    Created adapter classes.

---
+++ ApplyingtheAdapterPattern/src/main/java/adapters/LineAdapter.java
@@ -0,0 +1,56 @@
+package adapters;
+
+import drawing.IShape;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import shapes.Line;
+
+public class LineAdapter implements IShape{
+
+    Line line
+
+    @Override
+    public IShape setThickness(double value) {
+        return null;
+    }
+
+    @Override
+    public IShape setColor(Color value) {
+        return null;
+    }
+
+    @Override
+    public IShape setFilled(boolean value) {
+        return null;
+    }
+
+    @Override
+    public double getX() {
+       return line.getX();
+    }
+
+    @Override
+    public double getY() {
+        return line.getY();
+    }
+
+    @Override
+    public double getThickness() {
+        return 0;
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public boolean getFilled() {
+        return false;
+    }
+
+    @Override
+    public void drawShape(GraphicsContext graphics) {
+
+    }
+}
---
+++ ApplyingtheAdapterPattern/src/main/java/adapters/RectangleAdapter.java
@@ -0,0 +1,52 @@
+package adapters;
+
+import drawing.IShape;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class RectangleAdapter implements IShape{
+    @Override
+    public IShape setThickness(double value) {
+        return null;
+    }
+
+    @Override
+    public IShape setColor(Color value) {
+        return null;
+    }
+
+    @Override
+    public IShape setFilled(boolean value) {
+        return null;
+    }
+
+    @Override
+    public double getX() {
+        return 0;
+    }
+
+    @Override
+    public double getY() {
+        return 0;
+    }
+
+    @Override
+    public double getThickness() {
+        return 0;
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public boolean getFilled() {
+        return false;
+    }
+
+    @Override
+    public void drawShape(GraphicsContext graphics) {
+
+    }
+}
